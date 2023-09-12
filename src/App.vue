100d8e79e7bfa73deb71fddf34fd4332e198c648
Thalys Márcio <thalysmarciobn@icloud.com>
2023-09-12 08:50:07 -0300

    Pub

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <router-view/>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <div class="bg-gradient-to-b from-gray-900 to-gray-950 w-full h-full absolute top-0 left-0 z-0"></div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  height: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="gallery">
     <div class="p-[60px]">
-  <div class="flex items-center justify-center">
-    <div class="container">
-      <div class="mt-8 grid grid-cols-2 gap-2 rounded-xl p-2 lg:grid-cols-4 w-full h-full">
-        <div class="group relative overflow-hidden rounded-xl">
-          <div class="absolute inset-0 h-full w-full group-hover:bg-rose-400/20"></div>
-
-          <img src="https://i.imgur.com/SDjdMbe.png" class="h-auto w-full" alt="Nasi lemak cover" />
+      <div class="flex items-center justify-center">
+        <div class="container">
+          <router-link :to="{ name: 'home' }" class="relative inline-flex items-center justify-center mt-2 mb-10 h-16 px-10 py-0 text-xl font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline">
+            <span class="w-full">Voltar</span>
+          </router-link> 
+          <div class="mt-8 grid grid-cols-2 gap-2 rounded-xl p-2 lg:grid-cols-4 w-full h-full">
+            <transition-group name="fade" mode="out-in">
+              <div v-for="item in items" :key="item.id" class="group relative overflow-hidden rounded-xl">
+                <div class="absolute inset-0 h-full w-full group-hover:bg-gray-400/20"></div>
+                <img :src="item.image" class="h-auto w-full" />
+              </div>
+            </transition-group>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
   </div>
 </template>
 
@@ -29,10 +33,14 @@ export default {
       items: []
     }
   },
-  mounted() {
-    edgeConfig.get('arts').then((data) => {
-      console.log(data);
-    })
+  async mounted() {
+    const data = await edgeConfig.get('arts')
+
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i]
+      this.items.push(element)
+      await new Promise((resolve) => setTimeout(resolve, 100))
+    }
   }
 }
 
@@ -42,5 +50,12 @@ export default {
 .gallery {
   position: relative;
   z-index: 99;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active no VUE <2.1.8 */ {
+  opacity: 0;
 }
 </style>
